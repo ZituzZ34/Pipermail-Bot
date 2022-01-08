88d2c6c402f8dcc28f6aa687d95d239641f1f27d
ZituzZ34 <cracker.slow134@gmail.com>
2022-01-08 20:31:02 +0100

    Get chat id

--- src/bot/telegram/index.ts
+++ src/bot/telegram/index.ts
@@ -3,17 +3,22 @@ import { Telegraf } from "telegraf";
 import config from "../../config";
 
 import log from "../../helpers/log";
+import initHelpCommand from "./commands/help";
+
 
 const telegramBot = new Telegraf(config.token);
 
 function initTelegramBot() {
     log("Connecting bot to Telegram");
-
+    
     telegramBot.launch()
+    
     telegramBot.telegram.sendMessage(config.chatId, "Pipermail-Telegram Bot iniciado")
+    
+    initHelpCommand(telegramBot);
 
     log("Bot connected to Telegram");
-
+    
 }
 
 export {telegramBot, initTelegramBot}
